11469a861b40c8d946c86d44c4c9f2246266b876
oli182 <noeljeandupeux@msn.com>
2022-04-20 12:16:17 +0200

    Csv kopfzeile mit schreiben

--- kaifa.py
+++ kaifa.py
@@ -131,7 +131,7 @@ while 1:
     #CSV Schreiben
     if useCSV:
 
-        toWrite = [
+        toWrite = ["Wirkenergie+","Momentanleistung P+", "Strom L1", "Storm L2", "Strom L2"],
             [WirkenergieP, MomentanleistungP, StromL1, StromL2, StromL3]
         ]
 
