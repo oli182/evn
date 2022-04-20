9bc250a2b806f21188b40020e0f8a40ebb0cc251
oli182 <noeljeandupeux@msn.com>
2022-04-20 12:45:46 +0200

    deaktivieren print soup

--- kaifa.py
+++ kaifa.py
@@ -72,7 +72,7 @@ while 1:
     results_16 = soup.find_all('uint16')
     #print(results_16)
     #print(results_32)
-    print(soup)
+    #print(soup)
 
     #Wirkenergie A+ in KiloWattstunden
     WirkenergieP = int(str(results_32)[16:16+8],16)/1000
