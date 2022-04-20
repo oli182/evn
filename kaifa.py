013e4efc4e2cf514b8cb6c9fd9efd79730561325
oli182 <noeljeandupeux@msn.com>
2022-04-20 14:20:15 +0200

    z14 from löschen

--- kaifa.py
+++ kaifa.py
@@ -11,7 +11,7 @@ from gurux_dlms.GXDLMSTranslator import GXDLMSTranslator
 from gurux_dlms.GXDLMSTranslatorMessage import GXDLMSTranslatorMessage
 from bs4 import BeautifulSoup
 import csv
-from datetime import datetime
+import datetime
 
 
 # EVN Schlüssel eingeben zB. "36C66639E48A8CA4D6BC8B282A793BBB"
