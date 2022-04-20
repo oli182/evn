cb65b456eaf0a78ad5ab904c0d496349f7573337
oli182 <noeljeandupeux@msn.com>
2022-04-20 10:44:09 +0200

    csv erstellen

--- write_csv_test.py
+++ write_csv_test.py
@@ -1,2 +1,16 @@
-import typing_extensions
+import csv
+from logging.handlers import RotatingFileHandler
 
+toWrite = [
+    ["Datum","Höchster Wert", "Tiefster Wert"],
+    ["01.01.2020","10","3"],
+    ["02.02.2020","9","5"],
+]
+
+file = open('test_csv.csv', 'w')
+
+with file:
+    writer ? csv.writer(file)
+
+    for row in toWrite:
+        writer.writerrow(row)
